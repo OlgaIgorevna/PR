52234a3fb9481d4ffecffa5d1b44fb000f9a39d5
Olga Igorevna Pron <olga.pron.kld@gmail.com>
2017-12-29 17:40:21 +0200

    правки

--- js/script.js
+++ js/script.js
@@ -17,62 +17,30 @@
 $(window).on("load", function () {
   //$(window).load(function() {
   $(".consult").click(function () {
-    //открыть модальное окно с id="modalConsult"
     $("#modalConsult").modal('show');
   });
+
   $(".order").click(function () {
-    //открыть модальное окно с id="modalConsult"
     $("#modalOrder").modal('show');
   });
+
   $(".call").click(function () {
-    //открыть модальное окно с id="modalConsult"
     $("#modalCall").modal('show');
   });
 
-  //  $("#flexiselDemo1").flexisel();
-
-
-
-
-  $("#flexiselDemo2").flexisel({
-    visibleItems: 4,
-    itemsToScroll: 3,
-    animationSpeed: 200,
-    infinite: true,
-    navigationTargetSelector: null,
-    autoPlay: {
-      enable: false,
-      interval: 5000,
-      pauseOnHover: true
-    },
-    responsiveBreakpoints: {
-      portrait: {
-        changePoint: 480,
-        visibleItems: 1,
-        itemsToScroll: 1
-      },
-      landscape: {
-        changePoint: 640,
-        visibleItems: 2,
-        itemsToScroll: 2
-      },
-      tablet: {
-        changePoint: 768,
-        visibleItems: 3,
-        itemsToScroll: 3
-      }
-    },
-    loaded: function (object) {
-      console.log('Slider loaded...');
-    },
-    before: function (object) {
-      console.log('Before transition...');
-    },
-    after: function (object) {
-      console.log('After transition...');
-    },
-    resize: function (object) {
-      console.log('After resize...');
-    }
+  $('#modalConsult').on('shown.bs.modal', function () {
+    $('#inputName1').focus();
   });
+
+  $('#modalOrder').on('shown.bs.modal', function () {
+    $('#inputName2').focus();
+  });
+
+  $('#modalCall').on('shown.bs.modal', function () {
+    $('#inputName3').focus();
+  });
+  
+//  $('#inputName4').focus();
+
+
 });
