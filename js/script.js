e1df48a8a3ab7d19b48812cd9ae394e883eaf1a9
Olga Igorevna Pron <olga.pron.kld@gmail.com>
2018-01-08 17:52:35 +0200

    правки

--- js/script.js
+++ js/script.js
@@ -40,35 +40,87 @@ $(window).on("load", function () {
     $('#inputName3').focus();
   });
 
-//  $('#open-menu').click(function () {
-//    $("#menu").css("display", "block;");
-//  });
+  //  $('#open-menu').click(function () {
+  //    $("#menu").css("display", "block;");
+  //  });
 
-  
-   $('#open-menu').click(function( e ) {
-       /*
-           вместо $("selector").click( observer ) можно использовать синтаксис вида
-                  $("selector").on( 'click', observer );
-       */
-       e.preventDefault(); // отменяем стандартное поведение
 
-       // изменение стиля в нашем случае можно сделать так
-       // этот способ быстрее всего, но применять его лучше не надо
-       // хотя если сильно важна производительность - пишите так
-//       document.getElementByID("menu").style.display = 'block';
+  $('#open-menu').click(function (e) {
+    /*
+        вместо $("selector").click( observer ) можно использовать синтаксис вида
+               $("selector").on( 'click', observer );
+    */
+    e.preventDefault(); // отменяем стандартное поведение
 
-       // или так
-       $("#menu").css("display","block");
-       // или так
-//       $("#enterblock").show();
-   });
-  
-  
+    // изменение стиля в нашем случае можно сделать так
+    // этот способ быстрее всего, но применять его лучше не надо
+    // хотя если сильно важна производительность - пишите так
+    //       document.getElementByID("menu").style.display = 'block';
+
+    // или так
+    $("#menu").css("display", "block");
+    // или так
+    //       $("#enterblock").show();
+  });
+
+
+
+  $('#close-menu').click(function (e) {
+    e.preventDefault();
+    $("#menu").css("display", "none");
+  });
+
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true
+  });
+
+  $('a[data-slide]').click(function (e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+  });
+
+
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2       
+             }
+    },
+       {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1        
+             }
+    },
+       {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1        
+             }
+    }
+      
     
-   $('#close-menu').click(function( e ) {
-       e.preventDefault();
-       $("#menu").css("display","none");
-   });
-  
+      
+    ] 
+    
+  });
 
 });
